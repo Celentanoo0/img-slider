782feabff4a2df2d615d3a7347e78d0808ab7c5a
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2023-04-18 19:29:35 +0300

    project created

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'normalize.css'
+import './assets/reset.css'
+import './assets/general-styles.css'
+
+createApp(App).mount('#app')
